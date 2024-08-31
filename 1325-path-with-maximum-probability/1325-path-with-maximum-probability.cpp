64a56bfacb0c2aca9e3072f00986cbeca6171069
Vijaytech123 <vijay.vk4523@gmail.com>
2024-08-31 18:12:35 +0530

    Time: 129 ms (42.83%), Space: 68.8 MB (71.08%) - LeetHub

--- 1325-path-with-maximum-probability/1325-path-with-maximum-probability.cpp
+++ 1325-path-with-maximum-probability/1325-path-with-maximum-probability.cpp
@@ -1,28 +1,39 @@
 class Solution {
 public:
     double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start_node, int end_node) {
-        vector<double> maxProb(n, 0.0);
-        maxProb[start_node] = 1.0;
-
-        for (int i = 0; i < n - 1; ++i) {
-            bool updated = false;
-            for (int j = 0; j < edges.size(); ++j) {
-                int u = edges[j][0];
-                int v = edges[j][1];
-                double prob = succProb[j];
-
-                if (maxProb[u] * prob > maxProb[v]) {
-                    maxProb[v] = maxProb[u] * prob;
-                    updated = true;
-                }
-                if (maxProb[v] * prob > maxProb[u]) {
-                    maxProb[u] = maxProb[v] * prob;
-                    updated = true;
-                }
-            }
-            if (!updated) break;
+        vector<pair<int,double>>adj [n];
+        for(int i=0;i<edges.size();i++){
+                int no=edges[i][0];
+                int adj_no=edges[i][1];
+                adj[no].push_back({adj_no,succProb[i]}); 
+                adj[adj_no].push_back({no,succProb[i]}); // adj_node - prob
         }
 
-        return maxProb[end_node];
+        priority_queue<pair<double,int>>qu;
+        
+        qu.push({1.0,start_node});
+
+        vector<double>dis(n,INT_MIN);
+        dis[start_node]=1;
+
+        while(!qu.empty()){
+            int node=qu.top().second;
+            double dist=qu.top().first;
+            qu.pop();
+          
+
+            for(auto it : adj[node]){
+                int adj_node=it.first;
+                double cost=it.second;
+                if(dist*cost>dis[adj_node]){
+                    dis[adj_node]=dist*cost;
+                    qu.push({dis[adj_node],adj_node});
+                }
+            }
+        }
+
+        if(dis[end_node]==INT_MIN) return 0.0000;
+        else return dis[end_node];
+
     }
 };
